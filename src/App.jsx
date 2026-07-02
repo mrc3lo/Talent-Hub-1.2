74d085f3f37fdc72f0d5d83dadda61af59172e0d
Marcelo Diaz Flores <marcelo.diaz91@inacapmail.cl>
2026-07-01 21:53:10 -0400

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import EmployeesPage from './pages/EmployeesPage';
+
+/* 
+  Por ahora renderizamos directamente la página de empleados aquí.
+  Más adelante, el Integrante C colocará el "Router" en este archivo 
+  para manejar el Login y las demás secciones.
+*/
+function App() {
+  return (
+    <>
+      <EmployeesPage />
+    </>
+  );
+}
+
+export default App;
