fb04eaa4d244aab304a158a3167de22e341fc933
Marcelo Diaz Flores <marcelo.diaz91@inacapmail.cl>
2026-07-05 17:19:48 -0400

    avance previo a bd

---
+++ database/config.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb://localhost:27017/"
+DATABASE_NAME = "talent_hub"
+
+
+def get_database():
+    client = MongoClient(MONGO_URI)
+    return client[DATABASE_NAME]
---
+++ src/pages/EmployeeProfilePage.jsx
@@ -0,0 +1,19 @@
+export default function EmployeesPage() {
+    const [employees, setEmployees] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const user = {
+        name: 
+        email:
+        profilePicture:
+    };
+
+    useEffect(() => {}
+        )
+    }
+
+    return (
+        <div>
+            <h1>Employees</h1>
+            <p>This is the Employees page.</p>
+        </div>
+    );
